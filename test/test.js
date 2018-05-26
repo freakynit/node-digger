c0e130f15f426999982f0e842ef7d917958767b5
nitin <nitin@webklipper.com>
2018-05-26 22:40:44 +0530

    can now register error handler callback

--- test/test.js
+++ test/test.js
@@ -6,14 +6,18 @@ describe('digger()', function(){
         var data = {a: {b: {c: {d: 10}}}};
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level('a.b')
             .or(100)
             .dig()).to.deep.equal({c: {d: 10}});
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level(['a', 'b'])
+            .or(100)
+            .dig()).to.deep.equal({c: {d: 10}});
+
+        expect(new digger(data, 'a.b')
             .or(100)
             .dig()).to.deep.equal({c: {d: 10}});
 
@@ -29,17 +33,24 @@ describe('digger()', function(){
             .dig()).to.deep.equal({c: {d: 10}});
     });
 
+    it('should be able to use in one-shot mode', function(){
+        var data = {a: {b: {c: {d: 10}}}};
+
+        expect(digger.dig(data, 'a.b.c.d', 100)).to.be.equal(10);
+        expect(digger.dig(data, null, 100, "some error value")).to.be.equal("some error value");
+    });
+
     it('should be able to return leaf objects', function(){
         var data = {a: {b: {c: {d: 10}}}};
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level('a.b.c.d')
             .or(100)
             .dig()).to.be.equal(10);
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level(['a', 'b', 'c', 'd'])
             .or(100)
             .dig()).to.be.equal(10);
@@ -49,55 +60,49 @@ describe('digger()', function(){
         var data = {a: {b: {c: {d: 10}}}};
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level('a.b.c.d.e')
             .or(100)
             .dig()).to.be.equal(100);
 
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level('.')
             .or(100)
             .dig()).to.be.equal(100);
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level(['.'])
             .or(100)
             .dig()).to.be.equal(100);
 
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level('')
             .or(100)
             .dig()).to.be.equal(100);
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level([])
             .or(100)
             .dig()).to.be.equal(100);
 
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level('something')
             .or(100)
             .dig()).to.be.equal(100);
 
         expect(new digger()
-            .area(data)
+            .object(data)
             .level(['something'])
             .or(100)
             .dig()).to.be.equal(100);
-    });
-
-    it('should be able to use in one-shot mode', function(){
-        var data = {a: {b: {c: {d: 10}}}};
-
-        expect(digger.dig(data, 'a.b.c.d', 100)).to.be.equal(10);
     });
 
     it('should be able to throw exception on invalid/null input', function(){
@@ -105,7 +110,7 @@ describe('digger()', function(){
 
         var throwWrapper = function(d, v){
             new digger()
-                .area(d)
+                .object(d)
                 .level(v)
                 .or(100)
                 .dig()
@@ -113,6 +118,30 @@ describe('digger()', function(){
 
         expect(throwWrapper.bind(null, data, null)).to.throw();
         expect(throwWrapper.bind(null, data)).to.throw();
+    });
+
+    it('should not throw exception if error value is specified on invalid/null input', function(){
+        expect(new digger()
+            .object(null)
+            .level(null)
+            .or(100)
+            .onError("some error value")
+            .dig()).to.be.equal("some error value");
+    });
+
+    it('should call specified error handler function on invalid/null input', function(){
+        var data = {a: {b: {c: {d: 10}}}};
+
+        new digger()
+            .object(data)
+            .level(null)
+            .or(100)
+            .onError(function(err, data, level, defaultValue, errorValue){
+                console.log("onError callback invoked");
+                expect(err).to.not.equal(null);
+                expect(data).to.deep.equal(data);
+                expect(level).to.be.equal(null);
+            });
     });
 });
 
